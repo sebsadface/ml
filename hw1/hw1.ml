d3cde8bf396c1b26404c804b5da1cf2d60ebdfef
Sebastian Liu <sebsadface@gmail.com>
2023-03-28 15:41:04 -0700

    hw1 init

---
+++ hw1/hw1.ml
@@ -0,0 +1,26 @@
+(* CSE 341, Homework 1, Provided Code *)
+
+(* You might choose to uncomment these, like the lecture code does *)
+(* #utop_prompt_dummy
+   let _ = UTop.set_show_box false *)
+
+(* Use these functions to extract parts of a date *)
+let fst3 (x,_,_) = x (* gets the first element of a triple *)
+let snd3 (_,x,_) = x (* gets the second element of a triple *)
+let thd3 (_,_,x) = x (* gets the third element of a triple *)
+
+(**
+ * TODO: Complete the 12 function bindings described in the assignment.  For the first (2), 
+ * we have given you the correct first line and an incorrect function body.
+ *)
+
+ (* 1 *)
+let is_older ((date1 : int * int * int), (date2 : int * int * int)) =
+  false
+
+(* 2 *)
+let rec number_in_month ((dates : (int * int * int) list), (month : int)) =
+  0
+
+(* continue for 3 and onward here *)
+ 
