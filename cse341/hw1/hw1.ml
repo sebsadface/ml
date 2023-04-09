1c86bab913d376dc6f9648ee63df0463cacb3eb1
Sebastian Liu <sebsadface@gmail.com>
2023-04-08 21:35:32 -0700

    8 -12

--- cse341/hw1/hw1.ml
+++ cse341/hw1/hw1.ml
@@ -17,10 +17,7 @@ let thd3 (_,_,x) = x (* gets the third element of a triple *)
  (* 1 *)
 let is_older ((date1 : int * int * int), (date2 : int * int * int)) =
   if thd3(date1) = thd3(date2) then
-    if snd3(date1) = snd3(date2) then
-      fst3(date1) < fst3(date2)
-    else
-      snd3(date1) < snd3(date2)
+    if snd3(date1) = snd3(date2) then fst3(date1) < fst3(date2) else snd3(date1) < snd3(date2)
   else
     thd3(date1) < thd3(date2)
 
@@ -64,6 +61,45 @@ else
   get_nth(List.tl ls, n - 1)
 
 (* 7 *)
-let rec string_of_date (date : (int * int * int)) = 
+let string_of_date (date : (int * int * int)) = 
   let str_months = ["January"; "February"; "March"; "April"; "May"; "June"; "July"; "August"; "September"; "October"; "November"; "December"] in
     get_nth(str_months, snd3(date)) ^ "-" ^ string_of_int(fst3(date)) ^ "-" ^ string_of_int(thd3(date))
+
+(* 8 *)
+let rec number_before_reaching_sum ((sum : int), (ls : int list)) =
+  if sum - List.hd ls <= 0 then
+    0
+  else
+    1 + number_before_reaching_sum((sum - List.hd ls), (List.tl ls))
+
+(* 9 *)
+let what_month (day : int) = 
+  let months = [31; 28; 31; 30; 31; 30; 31; 31; 30; 31; 30; 31] in
+    1 + number_before_reaching_sum(day, months)
+
+(* 10 *)
+let rec month_range ((day1 : int), (day2 : int)) =
+  if day1 > day2 then
+    []
+  else
+    what_month(day1) :: month_range(day1 + 1, day2)
+
+(* 11 *)
+let rec oldest (dates : (int * int * int) list) =
+  if dates = [] then
+    None
+  else
+    let oldest_tl = oldest(List.tl dates) in
+      if Option.is_none oldest_tl || is_older (List.hd dates, Option.get oldest_tl) then Some (List.hd dates) else oldest_tl
+
+(* 12 *)
+let cumulative_sum (nums : int list) = 
+  let rec sum_helper ((lst : int list), (prev_sum : int)) =
+    if lst = [] then
+      []
+    else
+      let new_sum = List.hd lst + prev_sum in
+       new_sum :: sum_helper (List.tl lst, new_sum)
+  in
+  sum_helper(nums, 0)
+
